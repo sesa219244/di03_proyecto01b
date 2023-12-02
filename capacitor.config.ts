7f5e9b98ec93fe119ef64f97d689888cc5950130
ogarijo <ogarijo@birt.eus>
2023-12-02 18:34:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI03_Proyecto01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
